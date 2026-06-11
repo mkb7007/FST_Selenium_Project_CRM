7e1e7e458bdb39c197fb4eed389fe0a1fbef9d37
Mebin Kuriakose Baby <mebin.kuriakose.baby@ibm.com>
2026-06-11 16:33:00 +0530

    Login feature tested and validated

--- CRM_Project/src/test/java/project/FirstTest.java
+++ CRM_Project/src/test/java/project/FirstTest.java
@@ -1,7 +1,15 @@
 package project;
 
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,7 +19,7 @@ public class FirstTest {
 	//Instantiating driver
 	WebDriver driver;
 	
-	@BeforeClass
+	@BeforeClass(alwaysRun = true)
 	  public void setUp() {
 		
 		//Invoking fire fox driver
@@ -22,12 +30,40 @@ public class FirstTest {
 		
 	  }
 	
-  @Test
-  public void f() {
+  @Test(priority = 1)
+  public void verifyPage() {
+	  //Verifying page title
+	  assertEquals(driver.getTitle(), "SuiteCRM");
   }
   
 
-  @AfterClass
+  @Test(priority = 2)
+  public void loginTest() {
+	 
+	  //Adding explicit wait
+	 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	 //Finding the user-name field by waiting until user-name field appears
+	 WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder = 'Username']")));
+	  
+	//Sending value to user-name
+	  element.sendKeys("admin");
+	  
+	//Finding the password field and passing value
+	  driver.findElement(By.xpath("//input[@name = 'password']")).sendKeys("5Nx#I6BK%r3$8vz0ch");
+	  
+	  //clicking login
+	  driver.findElement(By.id("login-button")).click();
+	  
+	  //Verifying the status for successful login
+	  wait.until(ExpectedConditions.urlContains("home"));
+	  String currentUrl = driver.getCurrentUrl();
+	  String expectedUrl = "http://crm.local:3050/#/home";
+	  
+	  assertEquals(currentUrl, expectedUrl);
+	  
+  }
+
+  @AfterClass(alwaysRun = true)
   public void tearDown() {
 	  //Closing the browser
 	  driver.quit();
